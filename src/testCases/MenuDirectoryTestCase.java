f1495f93d3ae800aa01192fc4e6ebd69a092394f
Sonja Bitencourte <sonjabitencourte@gmail.com>
2018-05-18 20:04:54 -0300

    Ajustando testCase Directory

--- src/testCases/MenuDirectoryTestCase.java
+++ src/testCases/MenuDirectoryTestCase.java
@@ -2,6 +2,7 @@ package testCases;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.PageFactory;
 
 import objects.MenuDirectoryObjs;
@@ -9,17 +10,26 @@ import utils.WebDriverInstance;
 
 public class MenuDirectoryTestCase extends WebDriverInstance {
 	
-	LoginTestCase loginTestCase = PageFactory.initElements(driver, LoginTestCase.class);
 	MenuDirectoryObjs menuDirectoryObjs = PageFactory.initElements(driver, MenuDirectoryObjs.class);
 	
 	@Before
 	public void before() throws InterruptedException {
-		loginTestCase.login();
 		menuDirectoryObjs.buttonMenuDirectory(driver).click();
 	}
 	
 	@Test
-	public void timeSheetAdd() throws InterruptedException {
+	public void searchForLinda() throws InterruptedException {
 		Thread.sleep(1000);
+		menuDirectoryObjs.searchName(driver).sendKeys("Linda Anderson");
+		
+		menuDirectoryObjs.comboJobTitle(driver).click();
+		menuDirectoryObjs.comboJobTitle(driver).sendKeys("HR Manager");
+		menuDirectoryObjs.comboJobTitle(driver).sendKeys(Keys.RETURN);
+		
+		menuDirectoryObjs.comboLocation(driver).click();
+		menuDirectoryObjs.comboLocation(driver).sendKeys("Texas R&D");
+		menuDirectoryObjs.comboLocation(driver).sendKeys(Keys.RETURN);
+		
+		menuDirectoryObjs.buttonSearch(driver).click();
 	}
 	}
